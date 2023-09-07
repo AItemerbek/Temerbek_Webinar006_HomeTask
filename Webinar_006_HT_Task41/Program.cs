49798589a7ce8a6bc48068037547cad973c078c5
alekseytemerbek <tech.temerbek@gmail.com>
2023-09-07 21:55:55 +0300

    add Home task

---
+++ Webinar_006_HT_Task41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] InputArray(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"Введите число № {i + 1} = ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+    return array;
+}
+
+int CountPositiveNums(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+
+    }
+    return result;
+}
+
+Console.Clear();
+
+int numberOfNums = InputNum("Введите общее количество чисел: ");
+
+Console.WriteLine($"Количество чисел больше нуля, которые ввел пользователь равно {CountPositiveNums(InputArray(numberOfNums))}");
---
+++ Webinar_006_HT_Task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void IntersectionPoint(double b1Point, double k1Point, double b2Point, double k2Point)
+{
+    double x = 0;
+    double y = 0;
+    if (k1Point == k2Point) Console.WriteLine("Прямые линии паралельны или совпают");
+    else
+    {
+        x = (b2Point - b1Point) / (k1Point - k2Point);
+        y = k1Point * x + b1Point;
+        Console.WriteLine($"Точка пересечения двух прямых находятся в координатах ({x}; {y})");
+    }
+}
+
+Console.Clear();
+
+int b1 = InputNum("Введите b1 = ");
+int k1 = InputNum("Введите k1 = ");
+int b2 = InputNum("Введите b2 = ");
+int k2 = InputNum("Введите k2 = ");
+
+IntersectionPoint(b1, k1, b2, k2);
